1a155dfd9f58bffb870c4d918ee0347701311086
Trần Đức Anh <75102477+Helianthusss@users.noreply.github.com>
2024-08-09 10:15:58 +0700

    Delete back_end/app directory

--- back_end/app/api/utils.py
+++
@@ -1,7 +0,0 @@
-import torch
-import torch.nn.functional as F
-
-def compute_similarity(feature1, feature2):
-    # Cosine similarity
-    cos_sim = F.cosine_similarity(feature1, feature2)
-    return cos_sim
